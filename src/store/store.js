f7d9764b26744110d6a3310a9ed21116fbdeb68f
Petramanullang <petra.mainacc@gmail.com>
2023-12-16 11:59:22 +0700

    init

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,30 @@ import counterReducer from "../reducer/counterSlice";
 import toggleReducer from "../reducer/toggleSlice";
 import todoReducer from "../reducer/todoSlice";
 import todoByIdReducer from "../reducer/todoDynamicSlice";
+import registerReducer from "../reducer/registerSlice";
 
+const saveTokenMiddleware = () => (next) => (action) => {
+  if (action.type === "register/fetchRegisterUser/fulfilled") {
+    const response = action.payload;
+    const token = response?.token;
+    console.log("Middleware Response", response);
+    console.log("Middleware Token", token);
+    localStorage.setItem("token", token);
+  }
+  next(action);
+};
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     toggle: toggleReducer,
     todo: todoReducer,
-    todoById: todoByIdReducer
+    todoById: todoByIdReducer,
+    register: registerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(saveTokenMiddleware),
 });
+
+// export default store;
